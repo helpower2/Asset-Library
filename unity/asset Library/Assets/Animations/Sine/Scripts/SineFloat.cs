7f3ba3eb607369cdd9767c0f7fdf83c2474404ee
helpower2 <jeffreymonnink@gmail.com>
2019-04-10 15:58:03 +0200

    added sine
    added 2 sine scripts.
    added a todo list to pooler

---
+++ unity/asset Library/Assets/Animations/Sine/Scripts/SineFloat.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class SineEventFloat : UnityEvent<float>  { }
+public class SineFloat : MonoBehaviour
+{
+    public float sinePulseValue, speed;
+    public SineEventFloat Sine = new SineEventFloat();
+    public float Speed { get { return speed; } set { speed = value; } }
+    public float SinePulseValue { get { return sinePulseValue; } set { sinePulseValue = value; } }
+    private float time;
+
+    private void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        Sine.Invoke((CalculateSine(sinePulseValue, speed)));
+    }
+
+    public float CalculateSine(float _sinePulseValue, float _speed, float _baseSize = 1f)
+    {
+        return (_baseSize + Mathf.Sin(time * _speed) * _sinePulseValue / _speed);
+    }
+}
---
+++ unity/asset Library/Assets/Animations/Sine/Scripts/SineVector3.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class SineEventVector3 : UnityEvent<Vector3>  { }
+public enum Exes { X, Y, Z, XY, XZ, YZ, All}
+public class SineVector3 : MonoBehaviour
+{
+    [SerializeField] private float sinePulseValue, speed;
+    [SerializeField] private Exes exes;
+    public float Speed { get { return speed; } set { speed = value; } }
+    public float SinePulseValue { get { return sinePulseValue; }set { sinePulseValue = value; } }
+    public SineEventVector3 Sine = new SineEventVector3();
+
+    private float time;
+
+    private void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        switch (exes)
+        {
+            case Exes.X:
+                Sine.Invoke(Vector3.right * CalculateSine(sinePulseValue, speed));
+                break;
+            case Exes.Y:
+                Sine.Invoke(Vector3.up * CalculateSine(sinePulseValue, speed));
+                break;
+            case Exes.Z:
+                Sine.Invoke(Vector3.forward * CalculateSine(sinePulseValue, speed));
+                break;
+            case Exes.XY:
+                Sine.Invoke(new Vector3(1, 1, 0) * CalculateSine(sinePulseValue, speed));
+                break;
+            case Exes.XZ:
+                Sine.Invoke(new Vector3(1, 0, 1) * CalculateSine(sinePulseValue, speed));
+                break;
+            case Exes.YZ:
+                Sine.Invoke(new Vector3(0, 1, 1) * CalculateSine(sinePulseValue, speed));
+                break;
+            case Exes.All:
+                Sine.Invoke(Vector3.one * CalculateSine(sinePulseValue, speed));
+                break;
+        }
+        
+    }
+
+    public float CalculateSine(float _sinePulseValue, float _speed, float _baseSize = 1f)
+    {
+        return (_baseSize + Mathf.Sin(time * _speed) * _sinePulseValue / _speed);
+    }
+
+}
